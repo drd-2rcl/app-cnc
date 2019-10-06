03ee43cbb266f7a409218bbc6e3e624a65a15cb8
Diogo <drd.rclrd@gmail.com>
2019-10-06 15:26:47 -0300

    added styles in item for lists

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -56,5 +56,48 @@ const styles = StyleSheet.create({
 
   bold: {
     fontWeight: 'bold',
+  },
+
+  list: {
+    paddingHorizontal: 20,
+  },
+
+  listItem: {
+    marginRight: 15,
+  },
+
+  thumbnail: {
+    width: 200,
+    height: 120,
+    resizeMode: 'cover',
+    borderRadius: 2,
+  },
+
+  company: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginTop: 10,
+  },
+
+  price: {
+    fontSize: 15,
+    color: '#999',
+    marginTop: 5
+  },
+
+  button: {
+    height: 32,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+    marginTop: 15
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 15,
   }
 })
