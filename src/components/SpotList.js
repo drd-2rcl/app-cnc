679fe3c7e676ec80166b34c831ed3ec095893cf0
Diogo <drd.rclrd@gmail.com>
2019-10-06 15:01:28 -0300

    added logic for show list

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import api from  '../services/api';
 
@@ -21,6 +21,23 @@ export default function SpotList({ tech }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+      <FlatList 
+        style={styles.list}
+        data={spots}
+        keyExtractor={spot => spot._id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }}/>
+            <Text style={styles.company}>{item.company}</Text>
+            <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
+            <TouchableOpacity onPress={() => {}} style={styles.button}>
+              <Text style={styles.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   ) 
 }
