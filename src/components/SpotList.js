193db8d86ad9eae9b2b3a4b99cdb7ebd17bb5439
Diogo <drd.rclrd@gmail.com>
2019-10-06 14:38:47 -0300

    added style and function to get data

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import api from  '../services/api';
 
 export default function SpotList({ tech }) {
-  return <Text>{tech}</Text>
+  const [ spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const response = await api.get('/spots', {
+        params: { tech }
+      })
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+    </View>
+  ) 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+  },
+
+  title: {
+    fontSize: 20,
+    color: '#444',
+    paddingHorizontal: 20,
+    marginBottom: 15,
+  },
+
+  bold: {
+    fontWeight: 'bold',
+  }
+})
